71c3e041a82b88be5dbdfe5e31ce11da147a3e73
LENOVO <LENOVO@DESKTOP-L4OQDPO>
2025-06-13 23:56:40 +0530

    WH-4125 UPDATED THE COMMANDLINERUNNER class with LOG4J using SLF

--- src/main/java/com/example/github/runner/CommandLineRunner.java
+++ src/main/java/com/example/github/runner/CommandLineRunner.java
@@ -1,14 +1,25 @@
 package com.example.github.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import com.example.github.model.MessageRunner;
 
 @Component
 public class CommandLineRunner implements org.springframework.boot.CommandLineRunner {
-
+    private static Logger log= LoggerFactory.getLogger(MessageRunner.class);
 	@Override
 	public void run(String... args) throws Exception {
 	int a = 10;
-		System.out.println("WELCOME TO SPRING BOOT"+a);
+	try {
+		log.info("Started");
 	}
-
+	catch(Exception e) {
+		e.printStackTrace();
+	}
+		System.out.println("WELCOME TO SPRING BOOT"+a);
+    log.info("Ended");
+	}
+   
 }
